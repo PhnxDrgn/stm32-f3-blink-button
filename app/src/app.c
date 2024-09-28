69ee3f5a87692762b8e1a116b1c52851cd22a044
Andy Hoang <andyhoang2010@gmail.com>
2024-09-27 20:03:54 -0400

    button press turns on led for 500 ms

--- app/src/app.c
+++ app/src/app.c
@@ -5,9 +5,12 @@
 #include "stdbool.h"
 #include <stdint.h>
 
+uint32_t blinkTimeMillis = 500; // blink on time
+
 volatile bool buttonUpdate = false;
 bool buttonAction = false;
-volatile LED_STATE_t ledState = LED_OFF;
+LED_STATE_t ledState = LED_OFF;
+uint32_t blinkStateMillis = 0;
 BUTTON_t button;
 
 void APP_main()
@@ -25,19 +28,27 @@ void APP_main()
 
         if (buttonAction)
         {
-            switch (ledState)
+            // only blink if blink is not already in progress
+            if (ledState == LED_OFF)
             {
-            case LED_OFF:
-                ledState = LED_ON;
-                break;
+                // update start time for blink
+                blinkStateMillis = millis();
 
-            case LED_ON:
-                ledState = LED_OFF;
-                break;
+                // turn on LED
+                ledState = LED_ON;
+                LED_setState(ledState);
             }
 
-            LED_setState(ledState);
+            // reset action trigger
             buttonAction = false;
+        }
+
+        // check blink time to reset
+        if ((ledState == LED_ON) && ((millis() - blinkStateMillis) > blinkTimeMillis))
+        {
+            // turn on off
+            ledState = LED_OFF;
+            LED_setState(ledState);
         }
     }
 }
